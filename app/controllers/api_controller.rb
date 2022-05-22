6239f398a6fb2cb819c1bc09b514e93d62ca2dee
Gfreschi <gabrielfreschi@outlook.com>
2022-05-22 10:00:28 -0300

    "fix/current user via api call"

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,6 +4,7 @@ class ApiController < ApplicationController
   # equivalent of authenticate_user! on devise, but this one will check the oauth token
   # before_action :authenticate_user!
   before_action :doorkeeper_authorize!
+  before_action :current_user
 
   # Skip checking CSRF token authenticity for API requests.
   # protect_from_forgery with: CustomStrategy -> issue 
@@ -13,7 +14,7 @@ class ApiController < ApplicationController
   respond_to :json
 
   # helper method to access the current user from the token
-  def self.current_user
+  def current_user
     return unless doorkeeper_token
 
     @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
